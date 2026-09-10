026b02b4db3836faca3e47adf760e0c754b62d62
Hale Joel <princehalestudios@gmail.com>
2026-09-10 16:54:08 +0100

    Initialize intro.js

---
+++ intro.js
@@ -0,0 +1,9 @@
+const userDetails = [
+  {
+    name: "Hale Joel",
+    age: 30,
+    favoriteProgram: "Javascript",
+  },
+];
+
+console.log('My name is ' + userDetails[0].name + '.', 'I am ' + userDetails[0].age + ' years old.', 'My favorite programming language is ' + userDetails[0].favoriteProgram + '.');
